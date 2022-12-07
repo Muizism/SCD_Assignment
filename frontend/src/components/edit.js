9ec7be82c315f014286fa6389db7fb013a73ee48
Abdul Moiz <i200942@nu.edu.pk>
2022-12-07 10:40:52 +0500

    Deployed

--- frontend/src/components/edit.js
+++ frontend/src/components/edit.js
@@ -15,7 +15,7 @@ export default function Edit() {
  useEffect(() => {
    async function fetchData() {
      const id = params.id.toString();
-     const response = await fetch(`http://localhost:3001/record/${params.id.toString()}`);
+     const response = await fetch(`http://34.212.139.138:3001/record/${params.id.toString()}`);
  
      if (!response.ok) {
        const message = `An error has occurred: ${response.statusText}`;
@@ -54,7 +54,7 @@ export default function Edit() {
    };
  
    // This will send a post request to update the data in the database.
-   await fetch(`http://localhost:3001/update/${params.id}`, {
+   await fetch(`http://34.212.139.138:3001/update/${params.id}`, {
      method: "POST",
      body: JSON.stringify(editedPerson),
      headers: {
--- frontend/src/components/products.js
+++ frontend/src/components/products.js
@@ -24,7 +24,7 @@ export default function Create() {
    // When a post request is sent to the create url, we'll add a new record to the database.
    const newPerson = { ...form };
  
-   await fetch("http://localhost:3001/record/add", {
+   await fetch("http://34.212.139.138:3001/record/add", {
      method: "POST",
      headers: {
        "Content-Type": "application/json",
